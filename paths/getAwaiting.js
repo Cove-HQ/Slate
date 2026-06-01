1f603f1abbb57cbfc91fdb2ad35345d2fbeab26a
exorciqshun <ididntmeantoburnthesharpie@gmail.com>
2026-05-31 22:28:43 -0400

    fixed roscript to process the list/respond if it doesnt get it + made get awaiting a GET request rather than POST.

--- paths/getAwaiting.js
+++ paths/getAwaiting.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {get} = require('../manager.js');
 // Kick a user from the server
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ list: get() });
 })
 
