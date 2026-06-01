780a09cae3aad3d455f6ff926ca0096ee0cb00cc
exorciqshun <ididntmeantoburnthesharpie@gmail.com>
2026-05-31 23:03:51 -0400

    Moved app.listen underneath paths.

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Moderate with Slate.');
 });
-
-app.listen(port, () => {
-  console.log(`Slate is running (on ${port})`);
-});
-
-
-app.use(express.json());
 
 app.use("/actions/kick", require('./paths/kick.js'));
 app.use("/actions/warn", require('./paths/warn.js'));
 app.use("/actions/ban", require('./paths/ban.js'));
 app.use("/actions/process", require('./paths/process.js'));
 app.use("/processing/get", require('./paths/getAwaiting.js'));
+
+app.listen(port, () => {
+  console.log(`Slate is running (on ${port}mg of caffine/second)`);
+});
