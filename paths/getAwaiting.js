c9987158735a50f6bac26fec81f0c0c319ed6a28
exorciqshun <ididntmeantoburnthesharpie@gmail.com>
2026-05-31 23:18:45 -0400

    made getawaiting async and await to get data

--- paths/getAwaiting.js
+++ paths/getAwaiting.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {get} = require('../manager.js');
-// Kick a user from the server
-router.get('/', (req, res) => {
-    res.status(200).json({ list: get() });
+
+router.get('/', async (req, res) => {
+    const data = await get();
+    res.status(200).json({ list: data });
 })
 
 module.exports = router;
