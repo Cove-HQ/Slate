57bcbe2818a6e0c49342297e84397c27f7cf0687
exorciqshun <ididntmeantoburnthesharpie@gmail.com>
2026-06-01 01:40:14 -0400

    added a key system to ensure all requests are authorized + dotenv package to protect the key.
    updated roconfig and roscript to contain the most up to date and functional features + added .env to gitignore.

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Moderate with Slate.');
 });
+
+function verifyReq(req, res, next) {
+const key = req.body.key
+if(key !== process.env.key) return res.status(401).json({message: "Slate: You are not authorized to make this request. Please provide a valid key."});
+next();
+}
+
+app.post("*", verifyReq);
 
 app.use("/actions/kick", require('./paths/kick.js'));
 app.use("/actions/warn", require('./paths/warn.js'));
